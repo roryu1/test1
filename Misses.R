b360f257ca0b1beba7097cdcbeae1f0a5d7978cb
roryu1 <rory@metricstogether.com>
2023-01-18 10:05:50 -0700

    Rory played around with ggplot.

--- Misses.R
+++ Misses.R
@@ -1,23 +1,25 @@
 library(tidyverse)
 library(ggplot2)
-library(esquisse)
+
+#library(esquisse)
 
 ## fun website for R updates: https://www.r-bloggers.com/
 
-paste0("Misses My", c(22:25))
+#paste0("Misses My", c(22:25))
 
 
-cars <- tibble(mtcars)
-names(cars)
+#cars <- tibble(mtcars)
+#names(cars)
 
-ggplot(cars) +
-  aes(x = gear, y = mpg) +
-  geom_point(shape = "circle", size = 5, colour = "green") +
-  theme_minimal()
+#ggplot(cars) +
+  #aes(x = gear, y = mpg) +
+  #geom_point(shape = "circle", size = 5, colour = "green") +
+  #theme_minimal()
 
-flowers <- iris
-ggplot(iris) +
-  aes(x = Petal.Length, y = Petal.Width, colour = Species) +
-  geom_point(shape = "circle", size = 3) +
-  scale_color_hue(direction = 1) +
-  theme_minimal()
+#flowers <- iris
+#ggplot(iris) +
+  #aes(x = Petal.Length, y = Petal.Width, colour = Species) +
+  #geom_point(shape = "circle", size = 3) +
+  #scale_color_hue(direction = 1) +
+  #theme_minimal()
+
